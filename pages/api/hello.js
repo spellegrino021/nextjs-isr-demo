09f40ee956a3e5b053615072b1476378ade9fa2c
annikel <92825476+annikel@users.noreply.github.com>
2023-09-28 15:24:28 -0700

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Annina 4' })
+  res.status(200).json({ name: 'Annina 5' })
 }
